65fd2575c9987dc92f9b949b0527c9bf8ed355ad
GENTAXXX <bramantyaraka00@mail.ugm.ac.id>
2020-04-20 12:31:11 +0700

    First Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# UTS Hiya-hiya
+Muklas Rahmanto
+18/425973/SV/15115
+
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Firebase\JWT\JWT;
+use App\Http\Middleware\JwtMiddleware;
+use App\User;
+
+class UserController extends Controller
+{
+
+    public static function getCurrentUser(Request $request)
+    {
+        $credentials = JWT::decode($request->token, env('JWT_SECRET'), ['HS256']);
+        $user = User::find($credentials->sub);
+        return $user;
+    }
+
+    public function register(Request $request)
+    {
+        $result = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->input('password'))
+        ]);
+
+        if ($result) {
+            $data['code'] = 200;
+            $data['result'] = $result;
+        } else {
+            $data['code'] = 500;
+            $data['result'] = 'Error';
+        }
+        return response($data);
+    }
+
+    public function show($request)
+    {
+        $user = $this->getCurrentUser($request);
+        $result = User::where('id', $user->id)->first();
+
+        if ($result) {
+            $data['code'] = 200;
+            $data['result'] = $result;
+        } else {
+            $data['code'] = 500;
+            $data['result'] = 'Error';
+        }
+        return response()->json($data);
+    }
+
+    public function update(Request $request)
+    {
+        $user = $this->getCurrentUser($request);
+        $result = User::where('id', $user->id)->first();
+        $result->name = $request->input('name');
+        $result->email = $request->input('email');
+        $result->password = Hash::make($request->input('password'));
+        $result->save();
+
+        if ($result) {
+            $data['code'] = 200;
+            $data['result'] = $result;
+        } else {
+            $data['code'] = 500;
+            $data['result'] = 'Error';
+        }
+        return response()->json($data);
+    }
+
+    public function destroy($id)
+    {
+        $result = User::destroy('id', $id);
+
+        if ($result) {
+            $data['code'] = 200;
+            $data['result'] = $result;
+        } else {
+            $data['code'] = 500;
+            $data['result'] = 'Error';
+        }
+        return response()->json($data);
+    }
+}
